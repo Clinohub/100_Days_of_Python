43b3e8e24ce6e826a2d0e31a85afa1ed92565b0e
Clinohub <esmicorreo09@gmail.com>
2026-01-30 20:17:02 +0300

    Day 15-Coffee machine

--- Day_15/D15_Coffee_Maker.py
+++ Day_15/D15_Coffee_Maker.py
@@ -28,38 +28,73 @@ MENU = {
 }
 
 
-def invalid_order():
+water = 300
+milk = 200
+coffee = 100
+money = 0
+
+
+def ingredients(order, water, milk, coffee):
     """
-    Checks for proper order
+    Checks if availability of remaining ingredients
     """
-    global order
-    invalid = True
-    while invalid:
-        if order == 'off':
-            return 'Power off'
-        elif not order == 'espresso' or order == 'latte' or order == 'cappuccino' or order == 'report':
-            order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if water < MENU[order]['ingredients']['water']:
+        return 'water'
+    if order in {'latte', 'cappuccino'} and milk < MENU[order]['ingredients']['milk']:
+        return 'milk'
+    if coffee < MENU[order]['ingredients']['coffee']:
+        return 'coffee'
+    return None
+
+
+def main(water,milk,coffee,money):
+    power_on = True
+    while power_on:
+        order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if not order in {'espresso', 'latte', 'cappuccino', 'report', 'off'}:
+            print("Invalid choice,please try again.")
+        elif order == 'report':
+            print(f"Water: {water}ml")
+            print(f"Milk: {milk}ml")
+            print(f"Coffee: {coffee}g")
+            print(f"Money: ${money}")
         else:
-            return order
+            power_on = False
 
+    if order == 'off':
+        print("Power off")
+        return
 
-def report(serving):
-    """
-    Returns ingredients of coffee ordered
-    """
+    remaining_ing = ingredients(order, water, milk, coffee)
+    if remaining_ing in {'water', 'milk', 'coffee'}:
+        print(f"Sorry, there is not enough {remaining_ing}.")
+        main(water,milk,coffee,money)
+        return
 
+    water -= MENU[order]['ingredients']['water']
+    if order in {'latte', 'cappuccino'}:
+        milk -= MENU[order]['ingredients']['milk']
+    coffee -= MENU[order]['ingredients']['coffee']
 
-order = input("What would you like? (espresso/latte/cappuccino): ").lower()
-invalid_order()
+    print("Please insert coins.")
+    amt = []
+    denominations = ['quarters', 'dimes', 'nickels', 'pennies']
+    for denomination in denominations:
+        amt.append(int(input(f"How many {denomination}?: ")))
 
+    total_amt = amt[0]*0.25 + amt[1]*0.10 + amt[2]*0.05 + amt[3]*0.01
 
+    if total_amt < MENU[order]['cost']:
+        print("Sorry, that's not enough money. Money refunded.")
+        main(water,milk,coffee,money)
+        return
 
+    money += round(MENU[order]['cost'], 2)
+    change = round((total_amt - MENU[order]['cost']), 2)
 
+    print(f"Here is your ${change} in change.")
+    print(f"Here is your {order}. Enjoy!")
 
+    main(water,milk,coffee,money)
 
-
-
-
-
-
-
+main(water,milk,coffee,money)
