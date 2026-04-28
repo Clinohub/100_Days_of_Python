898c12eb11558078acb0890fb8937bbdad01c4c3
Clinohub <esmicorreo09@gmail.com>
2026-04-28 14:02:41 +0300

    update pong class

--- Day_22 - Pong Game/pong.py
+++ Day_22 - Pong Game/pong.py
@@ -10,12 +10,34 @@ class Pong(Turtle):
         self.shapesize(stretch_wid=1.0, stretch_len=1.0, outline=None) # pong ball diameter is 20px
         self.color('white')
         self.penup()
-        #self.speed("fastest")
+        self.reset()
+        self.speed(10)
 
     def move(self):
         self.fd(10)
 
-    def bounce(self):
+    def wall_bounce(self, heading):
         # no bounce if pong is almost parallel to paddle
-        if self.heading() < 87 and self.heading() > 93 or self.heading < 267 and self.heading() > 273: 
-            self.seth(360 - self.heading())
+        if heading >= 0 and heading <= 87: # 0 - 87
+            self.seth(360 - heading)
+
+        if heading >= 273 and heading < 360: # 273 - 359
+            self.seth(360 - heading)
+        
+        if heading >= 93 and heading <= 267: # 93 - 267
+            self.seth(360 - heading)
+            
+            
+
+    def paddle_bounce(self):
+        self.seth(180 - self.heading())
+
+    def reset(self, serving = "right"):
+        self.home() 
+        #self.drift = random.randint(-36, 37)
+        self.drift = 0
+
+        if serving == "right":
+            self.seth(self.drift)
+        else:
+            self.seth(self.drift - 180)
