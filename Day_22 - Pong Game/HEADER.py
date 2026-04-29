ab766efad47be6f85d04e940f109a8d691b114ad
Clinohub <esmicorreo09@gmail.com>
2026-04-29 08:41:52 +0300

    constants definition file

---
+++ Day_22 - Pong Game/HEADER.py
@@ -0,0 +1,34 @@
+# CONSTANTS
+
+class Header():
+    
+
+    DELAY_SPEED = 0.1
+
+    FONT_SIZE = 50
+
+    # angle limit for pong to stay within screen sides
+    HIGH_RIGHT_ANGLE = 36
+
+    OFFSET = 20
+
+    PADDLE_PACE = 30
+
+    # Pong and Paddle width padding - 20
+    # Paddle height padding - 60
+    PADDING = (20,60)
+
+    POINT = 3
+    PONG_PACE = 10
+
+    SCREENSIZE_HEIGHT = 600
+    SCREENSIZE_WIDTH = 800
+
+    # the lower the value, the faster the speed , between 0 and 1
+    SPEED_INCREMENT = 0.95
+
+    
+
+
+
+
