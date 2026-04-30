8f580e920941a4563a1176221e6e946ff44713d1
Clinohub <esmicorreo09@gmail.com>
2026-04-30 09:14:08 +0300

    recitation - lists

---
+++ xChallenges/R05_problems.py
@@ -0,0 +1,66 @@
+# Problem 1: Given a list of numbers. Write a function to turn every item of 
+# a list into its square.
+def square_list(my_list):
+    square_my_list = []
+    for elem in my_list:
+        square_my_list.append(elem * elem)
+        
+    return square_my_list
+
+# test
+print(square_list([1, 2, 3, 4]))
+print(square_list([10, 12, 13]))
+
+
+
+# Problem 2: Write a Python program to concatenate element-wise 
+# three given lists of same length
+# Original lists:
+list1 = ['0', '1', '2', '3', '4']
+list2 = ['red', 'green', 'black', 'blue', 'white']
+list3 = ['100', '200', '300', '400', '500']
+# Expected output : ['0red100', '1green200', '2black300', '3blue400', '4white500']
+
+def concatenate_lists(list_a, list_b, list_c):
+    great_list = []
+    for i in range(len(list_a)):
+        one_str = list_a[i] + list_b[i] + list_c[i]
+        great_list.append(one_str)
+        
+    return great_list
+
+# test
+print(concatenate_lists(list1, list2, list3))
+
+
+
+
+# Problem 3: Write a function to shift a given list to the right or left 
+# direction by a specified amount. Direction, rotation amount, and a 
+# list of integers should be inputs to the function.
+# e.g. 
+# Input list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Rotate the input list in left direction by 2:
+# [3, 4, 5, 6, 7, 8, 9, 10, 1, 2]
+# Rotate the input list in Right direction by 4:
+# [7, 8, 9, 10, 1, 2, 3, 4, 5, 6]
+
+# edit this to be "right" or "left"
+direction = "right" 
+
+def rotate_list(input_list, direction, shift):
+    start = shift % len(input_list)
+    
+    if direction == "right":
+        start *= -1
+        
+    shift_list = input_list[start::] + input_list[:start:]
+    
+    return shift_list
+
+# test 
+input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(rotate_list(input_list, "right", 14))
+print(rotate_list(input_list, "left", 4))
+
+
