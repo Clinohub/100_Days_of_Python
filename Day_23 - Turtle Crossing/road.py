eee38bc080aa23ee1484bf50c2a4f5d73997e471
Clinohub <esmicorreo09@gmail.com>
2026-05-07 07:12:47 +0300

    update Road class

--- Day_23 - Turtle Crossing/road.py
+++ Day_23 - Turtle Crossing/road.py
@@ -1,6 +1,5 @@
 
 from turtle import Turtle
-from main import WIDTH,HEIGHT,OFFSET
 
 class Road(Turtle):
 
@@ -8,17 +7,20 @@ class Road(Turtle):
         super().__init__()
         self.hideturtle()
         self.color("black")
+        self.pensize(5)
         self.start_line()
         self.finish_line()
 
+
     def start_line(self):
         self.penup()
-        self.goto(WIDTH//2 , HEIGHT//-2 + OFFSET)
+        self.goto(300, -255)
         self.pendown()
-        self.setx(WIDTH//-2)
+        self.setx(-300)
+
 
     def finish_line(self):
         self.penup()
-        self.goto(WIDTH//2, HEIGHT//2 - OFFSET)
+        self.goto(300, 255)
         self.pendown()
-        self.setx(WIDTH // -2)
+        self.setx(-300)
