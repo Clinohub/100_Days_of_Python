18f1d7309bdc18f38db53fc59292dafebae62c8e
Clinohub <esmicorreo09@gmail.com>
2026-05-07 07:09:00 +0300

    turtle crossing

---
+++ Day_23 - Turtle Crossing/How to play.md
@@ -0,0 +1,14 @@
+# WELCOME TO TURTLE CROSSING GAME
+
+> In this game, we have a player character represented by the 
+> turtle. We have cars represented by the different coloured 
+> boxes which moves from right to left.
+
+- You are supposed to cross the turtle, navigating past the 
+traffic successfully without being hit to play as long as it takes.
+- On crossing the road, you will be brought back into the starting
+ line, with an increase in difficulty-sped up cars.
+- If hit the GAME OVER.
+
+## CONTROLS
+**Use the "Up" arrow key to move the turtle across.**
--- Day_23 - Turtle Crossing/main.py
+++ Day_23 - Turtle Crossing/main.py
@@ -4,24 +4,50 @@
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from road import Road
 from scoreboard import Scoreboard
 import time
 
-WIDTH, HEIGHT = 600,600
-OFFSET = 20
-
+# screen setup
 screen = Screen()
-screen.setup(WIDTH, HEIGHT)
-screen.tracer(0)
+screen.setup(600, 600)
+screen.tracer(0) # turn animation off
 
 player = Player()
+road = Road()
 car_manager = CarManager()
-scoreboard = Scoreboard()
+score = Scoreboard()
 
+# player controls
+screen.listen()
+screen.onkey(player.cross, "Up")
+
+# game loop initialization
 play = True
 while play:
     time.sleep(0.1)
-    screen.update()
+    screen.update() # updates screen when animation is off
+
+    car_manager.move_cars()
+    car_manager.car_is_past_screen_reset()
+
+    # creates car traffic
+    if car_manager.generate_car:
+        if car_manager.car_distant():
+            car_manager.cars()
+
+    # updates score and speeds up the traffic - increases level
+    if player.ycor() > 270:
+        score.clear_scoreboard()
+        score.update_score()
+        player.reset_pos()
+        car_manager.speed_up()
+
+    # ends game
+    for cars in car_manager.traffic:
+        if abs(player.xcor() - cars.xcor()) <= 20 and abs(player.ycor() - cars.ycor()) <= 10:
+            score.game_over()
+            play = False
 
 
 screen.exitonclick()
